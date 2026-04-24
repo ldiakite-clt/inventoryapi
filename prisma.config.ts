5c3e9e1289605b4f5ed34ed04d5bfa4cb8d5b45f
RJ45 <ldiakite@charlotte.edu>
2026-04-24 13:05:58 -0400

    Final changes + added render URL

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: 'node --env-file=.env prisma/seed.js',
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
